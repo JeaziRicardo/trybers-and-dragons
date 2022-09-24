e0e631bd2153aa2b9b10a75bbf2e03d798e8c9c1
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-24 14:32:58 -0300

    feat: permite a leitura de name e dexterity

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -6,4 +6,12 @@ export default abstract class Race {
     this._name = name;
     this._dexterity = dexterity;
   }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
 }
