028c2bc00a2e94c355f86849d1eb1bde7301c9f9
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-25 22:37:06 -0300

    feat: implementa os métodos de SimpleFighter na classe Monster

--- src/Monster.ts
+++ src/Monster.ts
@@ -16,4 +16,16 @@ export default class Monster implements SimpleFighter {
   get strength(): number {
     return this._strength;
   }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
 }
