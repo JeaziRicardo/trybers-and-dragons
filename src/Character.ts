225b40e87d63423e9710ac4c182ac41760543a86
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-26 22:49:51 -0300

    feat: cria a classe Character com os atributos

---
+++ src/Character.ts
@@ -0,0 +1,31 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+export default class Character {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+  private _name: string;
+
+  constructor(name: string) {
+    this._dexterity = getRandomInt(1, 10);
+    this._name = name;
+    this._race = new Elf(this._name, this._dexterity);
+    this._archetype = new Mage(this._name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
+}
